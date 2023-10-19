9f56da2834cdab11b7cce90f49b2b33a5fd08267
Charlie Greco <cgreco812@gmail.com>
2023-10-19 18:40:43 -0400

    added getPosts method to Database class to get all of the posts from the database in descending order

--- lib/database.dart
+++ lib/database.dart
@@ -4,4 +4,13 @@ class DataBase {
   //Get a reference of the posts collection from the database
   static final CollectionReference posts =
       FirebaseFirestore.instance.collection('posts');
+
+  //Post
+  Stream<QuerySnapshot> getPosts() {
+    final orderedPosts = FirebaseFirestore.instance
+        .collection('posts')
+        .orderBy('timeStamp', descending: false)
+        .snapshots();
+    return orderedPosts;
+  }
 }
