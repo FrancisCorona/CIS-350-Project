9c8a4d21552e0f94845005fca5e46a542ce3fc92
ianstewart <ianastewart1021@gmail.com>
2023-10-23 22:59:37 -0400

    modified post code to store the input text as a string

--- lib/post.dart
+++ lib/post.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
+class PostPage extends StatefulWidget {
+  @override
+  _PostPageState createState() => _PostPageState();
+}
+
 // Creating a class called PostPage that extends StatelessWidget.
-class PostPage extends StatelessWidget {
+class _PostPageState extends State<PostPage> {
+  String postContents = 'null';
+  late String text;
+
   @override
   // Override the build method to define the widget's structure and appearance.
   Widget build(BuildContext context) {
@@ -12,33 +20,38 @@ class PostPage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          
+          children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TextField(
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: "Write your post",
                   border: InputBorder.none,
                 ),
                 maxLines: null,
+                onChanged: (newText) {
+                    postContents = newText;
+                },
               ),
             ),
-            Spacer(),
+            const Spacer(),
 
             Container(
-              margin: EdgeInsets.only(bottom: 20.0),
+              margin: const EdgeInsets.only(bottom: 20.0),
               child: ElevatedButton(
                 onPressed: () {
+                  setState(() {
+                    text = postContents;
+                  });
                   Navigator.of(context).pop();
                 },
                 style: ElevatedButton.styleFrom(
-                  minimumSize: Size(150, 60),
-                  textStyle: TextStyle(fontSize: 24.0),
+                  minimumSize: const Size(150, 60),
+                  textStyle: const TextStyle(fontSize: 24.0),
                 ),
                 child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 5.0),
-                  child: Text("Post"),
+                  padding: const EdgeInsets.symmetric(vertical: 5.0),
+                  child: const Text("Post"),
                 ),
               ),
             ),
