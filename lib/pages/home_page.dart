ad6d1755a4aaaa196cebf278b104104d1678ea06
Charlie Greco <cgreco812@gmail.com>
2023-10-25 21:34:19 -0400

    updated home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lakervent/main.dart';
-import 'package:lakervent/database.dart';
+import 'package:lakervent/components/database.dart';
 
 class HomePage extends State<Home> {
   var server = DataBase();
