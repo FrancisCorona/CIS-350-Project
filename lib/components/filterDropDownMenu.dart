ebacdfd11fb1abe90c59722cd3d97f380e8f9b12
ianstewart <ianastewart1021@gmail.com>
2023-10-28 17:46:55 -0400

    Changed return type to Container

--- lib/components/filterDropDownMenu.dart
+++ lib/components/filterDropDownMenu.dart
@@ -14,59 +14,56 @@ class _FilterDropDownMenuState extends State<FilterDropDownMenu> {
   String dropdownValue = list.first;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Center(
-        child: DropdownButtonHideUnderline(
-          child: DropdownButton2<String>(
-            alignment: AlignmentDirectional.center,
-            isExpanded: true,
-            value: dropdownValue,
-            //When a new item is selected switch to that item
-            onChanged: (String? value) {
-              setState(() {
-                dropdownValue = value!;
-              });
-              widget.onFilterChanged(dropdownValue);
-            },
-            buttonStyleData: ButtonStyleData(
-              width: 135,
-              padding: const EdgeInsets.only(right:6),
-              decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(20),
-                border: Border.all(
-                  color: Colors.black12,
-                ),
-              )
-            ),
-            iconStyleData: const IconStyleData(
-              icon: Icon(
-                Icons.filter_list_alt,
-                color: Color(0x80000000),
-                size: 25.0,
+    return Container(
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton2<String>(
+          alignment: AlignmentDirectional.center,
+          isExpanded: true,
+          value: dropdownValue,
+          //When a new item is selected switch to that item
+          onChanged: (String? value) {
+            setState(() {
+              dropdownValue = value!;
+            });
+            widget.onFilterChanged(dropdownValue);
+          },
+          buttonStyleData: ButtonStyleData(
+            width: 135,
+            padding: const EdgeInsets.only(right:6),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(
+                color: Colors.black12,
               ),
-            ),
-            dropdownStyleData: DropdownStyleData(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-              )
-            ),
-            //Converts the list to a map with the key and value being the same
-            items: list.map<DropdownMenuItem<String>>((value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Center(
-                  child: Text(
-                    value,
-                    style: TextStyle(
-                      color: Colors.black.withOpacity(0.6),
-                    )
-                  ),
-                ),
-              );
-            }).toList(),
+            )
           ),
+          iconStyleData: const IconStyleData(
+            icon: Icon(
+              Icons.filter_list_alt,
+              color: Color(0x80000000),
+              size: 25.0,
+            ),
+          ),
+          dropdownStyleData: DropdownStyleData(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+            )
+          ),
+          //Converts the list to a map with the key and value being the same
+          items: list.map<DropdownMenuItem<String>>((value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Center(
+                child: Text(
+                  value,
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.6),
+                  )
+                ),
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
