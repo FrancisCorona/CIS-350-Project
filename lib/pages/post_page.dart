f892675f10e0b5a467c5cdd3f812af32628dba5f
Francis Corona <thefranciscorona@gmail.com>
2023-10-29 17:22:58 -0400

    Refactored code by breaking it up into functions

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -15,64 +15,76 @@ class _PostPageState extends State<PostPage> {
   Widget build(BuildContext context) {
     // This provides a basic app structure with an app bar and body.
     return Scaffold(
-      // Set the app bar title.
-      appBar: AppBar(
-        backgroundColor: Color(0xFF00B9FF),
-        centerTitle: true,
-        elevation: 0,
-        iconTheme: IconThemeData(color: Color(0xFF0032A0)),
-        title: const Text("Create a Post",
-            style: TextStyle(
-              color: Color(0xFF0032A0),
-              fontSize: 25,
-              fontWeight: FontWeight.w600,
-            )),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: const InputDecoration(
-                  labelText: "Write your post",
-                  border: InputBorder.none,
-                ),
-                maxLines: null,
-                autofocus: true,
-                onChanged: (newText) {
-                  postContents = newText;
-                },
-              ),
-            ),
-            const Spacer(),
-            Container(
-              margin: const EdgeInsets.only(bottom: 20.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    text = postContents;
-                  });
-                  Navigator.of(context).pop(postContents);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF0032a0),
-                  minimumSize: const Size(220, 50),
-                  textStyle: const TextStyle(fontSize: 24.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(10.0), // Add rounded corners
-                  ),
-                ),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 5.0),
-                  child: const Text("Post"),
-                ),
-              ),
-            ),
-          ],
+      appBar: buildAppBar(),
+      body: buildPostContent(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Color(0xFF00B9FF),
+      centerTitle: true,
+      elevation: 0,
+      iconTheme: IconThemeData(color: Color(0xFF0032A0)),
+      title: Text(
+        "Create a Post",
+        style: TextStyle(
+          color: Color(0xFF0032A0),
+          fontSize: 25,
+          fontWeight: FontWeight.w600,
         ),
+      ),
+    );
+  }
+
+  Widget buildPostContent() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          buildPostInputField(),
+          const Spacer(),
+          buildPostButton(),
+        ],
+      ),
+    );
+  }
+
+  TextField buildPostInputField() {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: "Write your post",
+        border: InputBorder.none,
+      ),
+      maxLines: null,
+      autofocus: true,
+      onChanged: (newText) {
+        setState(() {
+          postContents = newText;
+        });
+      },
+    );
+  }
+
+  ElevatedButton buildPostButton() {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          text = postContents;
+        });
+        Navigator.of(context).pop(postContents);
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xFF0032a0),
+        minimumSize: const Size(220, 50),
+        textStyle: const TextStyle(fontSize: 24.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: const Text("Post"),
       ),
     );
   }
