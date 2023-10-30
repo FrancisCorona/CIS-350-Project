ea2c8dba3841cd58454144a667384f00123da0be
Francis Corona <thefranciscorona@gmail.com>
2023-10-29 21:48:16 -0400

    Fixed widget state implementation commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,25 @@ void main() async {
   ));
 }
 
+//Define a StatefulWidget class
 class Home extends StatefulWidget {
+  // Constructor for Home class
   const Home({Key? key}); 
 
-  @override
-  State<StatefulWidget> createState() {
-    return HomePage();
+// Override createState method
+@override
+  _HomeState createState() {
+    //Return an instance of _HomeState class
+    return _HomeState();
+  }
+}
 
+// Define a private state class _HomeState that extends State<Home>
+class _HomeState extends State<Home> {
+  // Override build method to define UI
+  @override
+  Widget build(BuildContext context) {
+    // Return instance of HomePage
+    return HomePage();
   }
 }
