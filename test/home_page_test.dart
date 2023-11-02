385793821cb170ff64862e1fd04fe1bab490970c
Francis Corona <thefranciscorona@gmail.com>
2023-11-02 11:01:55 -0400

    Test new post button goes to post page commit

--- test/home_page_test.dart
+++ test/home_page_test.dart
@@ -13,6 +13,19 @@ void main() {
     expect(find.text('LakerVent'), findsOneWidget);
   });
 
+  // Test New Post button goes to the Post page.
+  testWidgets('Test if "New Post" button navigates to PostPage', (WidgetTester tester) async {
+  // Build widget.
+  await tester.pumpWidget(MaterialApp(home: HomePage()));
+
+  // Simulate tap on button.
+  await tester.tap(find.text("New Post"));
+  await tester.pumpAndSettle();
+
+  // Check if Create a New Post is on the screen.
+  expect(find.text("Create a New Post"), findsOneWidget); // Replace with the text you expect on the PostPage
+});
+
 }
 
 
