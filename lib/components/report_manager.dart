c1966f5353f50389a394efc4753f5ca2de99aa60
Francis Corona <thefranciscorona@gmail.com>
2023-11-08 18:07:04 -0500

    isUnreported, remove ID from unreportedPosts

--- lib/components/report_manager.dart
+++ lib/components/report_manager.dart
@@ -8,16 +8,24 @@ class ReportManager {
   static final CollectionReference postsCollection = server.getPostCollection();
 
   static const _reportedPostsKey = 'reported_posts';
+  static const _unreportedPostsKey = 'unreported_posts';
 
   static Future<void> reportPost(String postID) async {
     final prefs = await SharedPreferences.getInstance();
     final reportedPosts = prefs.getStringList(_reportedPostsKey) ?? [];
+    final unreportedPosts = prefs.getStringList(_unreportedPostsKey) ?? [];
 
     if (!reportedPosts.contains(postID)) {
       reportedPosts.add(postID);
       await prefs.setStringList(_reportedPostsKey, reportedPosts);
       await server.reportPost(postID);
       await server.report(postID);
+    }
+
+    // Remove postID from unreportedPosts if it was there
+    if (unreportedPosts.contains(postID)) {
+      unreportedPosts.remove(postID);
+      await prefs.setStringList(_unreportedPostsKey, unreportedPosts);
     }
   }
 
@@ -29,11 +37,24 @@ class ReportManager {
       reportedPosts.remove(postID);
       await prefs.setStringList(_reportedPostsKey, reportedPosts);
     }
+
+    // Add postID to unreportedPosts
+    final unreportedPosts = prefs.getStringList(_unreportedPostsKey) ?? [];
+    if (!unreportedPosts.contains(postID)) {
+      unreportedPosts.add(postID);
+      await prefs.setStringList(_unreportedPostsKey, unreportedPosts);
+    }
   }
 
   static Future<bool> isReported(String postID) async {
     final prefs = await SharedPreferences.getInstance();
     final reportedPosts = prefs.getStringList(_reportedPostsKey) ?? [];
     return reportedPosts.contains(postID);
+  }
+
+  static Future<bool> isUnreported(String postID) async {
+    final prefs = await SharedPreferences.getInstance();
+    final unreportedPosts = prefs.getStringList(_unreportedPostsKey) ?? [];
+    return unreportedPosts.contains(postID);
   }
 }
