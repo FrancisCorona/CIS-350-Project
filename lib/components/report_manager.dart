0981ec22cc90d78be0ac65c20e1e77b28a9348d4
Francis Corona <thefranciscorona@gmail.com>
2023-11-08 01:04:30 -0500

    report posts shared preferences managing reports

--- lib/components/report_manager.dart
+++ lib/components/report_manager.dart
@@ -1,36 +1,39 @@
-// import 'dart:async';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'database.dart';
+import 'dart:async';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'database.dart';
 
-// class ReportManager {
-//   static final server = DataBase.getInstance();
-//   static final CollectionReference postsCollection = server.getPostCollection();
+class ReportManager {
+  static final server = DataBase.getInstance();
+  static final CollectionReference postsCollection = server.getPostCollection();
 
-//   static const _reportedPostsKey = 'reported_posts';
+  static const _reportedPostsKey = 'reported_posts';
 
-//   static Future<void> reportPost(String postID) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     final reportedPosts = prefs.getStringList(_reportedPostsKey) ?? [];
+  static Future<void> reportPost(String postID) async {
+    final prefs = await SharedPreferences.getInstance();
+    final reportedPosts = prefs.getStringList(_reportedPostsKey) ?? [];
 
-//     if (!reportedPosts.contains(postID)) {
-//       reportedPosts.add(postID);
-//       await prefs.setStringList(_reportedPostsKey, reportedPosts);
-//       await server.reportPost(postID);
-//     }
-//   }
+    if (!reportedPosts.contains(postID)) {
+      reportedPosts.add(postID);
+      await prefs.setStringList(_reportedPostsKey, reportedPosts);
+      await server.reportPost(postID);
+      await server.report(postID);
+    }
+  }
 
-//   static Future<void unreportPost(String postID) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     final reportedPosts = prefs.getStringList(_reportedPostsKey) ?? [];
-//     reportedPosts.remove(postID);
-//     await prefs.setStringList(_reportedPostsKey, reportedPosts);
-//     await server.unreportPost(postID);
-//   }
+  static Future<void> unreportPost(String postID) async {
+    final prefs = await SharedPreferences.getInstance();
+    final reportedPosts = prefs.getStringList(_reportedPostsKey) ?? [];
 
-//   static Future<bool> isReported(String postID) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     final reportedPosts = prefs.getStringList(_reportedPostsKey) ?? [];
-//     return reportedPosts.contains(postID);
-//   }
-// }
+    if (reportedPosts.contains(postID)) {
+      reportedPosts.remove(postID);
+      await prefs.setStringList(_reportedPostsKey, reportedPosts);
+    }
+  }
+
+  static Future<bool> isReported(String postID) async {
+    final prefs = await SharedPreferences.getInstance();
+    final reportedPosts = prefs.getStringList(_reportedPostsKey) ?? [];
+    return reportedPosts.contains(postID);
+  }
+}
